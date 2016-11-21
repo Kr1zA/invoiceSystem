b49ec93ea48fa1596e6888316c801a0d1c84a6ba
Maros <maros.koval@student.upjs.sk>
2016-11-21 02:02:06 +0100

    getCompanies implementovane ale neje otestovane.. vymazane daco

--- InvoiceSystem/src/test/java/sk/upjs/invoicesystem/CompaniesListTest.java
+++ InvoiceSystem/src/test/java/sk/upjs/invoicesystem/CompaniesListTest.java
@@ -12,7 +12,7 @@ public class CompaniesListTest {
     @Test
     public void testAddInvoice() {
         CompaniesList companiesList = CompaniesList.INSTANCE;
-        Company company = new Company("asd","","", "asd", "asd", 43242, "Slovensko", 234234L, 234234L, 13546L, "343434", "asd@asd.sd", "34sfasfa");
+        Company company = new Company("asd","asd", "asd", 43242, "Slovensko", 234234L, 234234L, 13546L, "343434", "asd@asd.sd", "34sfasfa");
         int companiesCount = companiesList.getCompanies().size();
         companiesList.addCompany(company);
         Assert.assertEquals(companiesCount + 1, companiesList.getCompanies().size());
@@ -21,7 +21,6 @@ public class CompaniesListTest {
     @Test
     public void testSearchCompany() {
         Assert.assertEquals(CompaniesList.INSTANCE.searchCompany("asd"), CompaniesList.INSTANCE.getCompanies().get(2));
-        Assert.assertEquals(CompaniesList.INSTANCE.searchCompany("meno", "priezvisko"), CompaniesList.INSTANCE.getCompanies().get(1));
     }
 
 }
