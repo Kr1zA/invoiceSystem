183e0560fc51977a1a398fe8072ebe88c842003c
Richard Staňa <richard.stana@student.upjs.sk>
2016-12-06 07:40:29 +0100

    test repaired

--- InvoiceSystem/src/test/java/sk/upjs/invoicesystem/MongoCompaniesTest.java
+++ InvoiceSystem/src/test/java/sk/upjs/invoicesystem/MongoCompaniesTest.java
@@ -5,6 +5,7 @@
  */
 package sk.upjs.invoicesystem;
 
+import java.util.List;
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,39 +16,39 @@ import static org.junit.Assert.*;
  * @author marosi
  */
 public class MongoCompaniesTest {
-    
+
     public MongoCompaniesTest() {
     }
-    
+
     @Before
     public void setUp() {
     }
 
     @Test
     public void testAddCompany() {
-        Company company = new Company("asd","asd", "asd", 43242, "Slovensko", 234234L, 234234L, 46579L, "343434", "asd@asd.sd", "34sfasfa");
-        CompaniesDao mongo = new CompaniesFactory().getCompanyDao();
-        long daco=mongo.size();
+        Company company = new Company("asd", "asd", "asd", 43242, "Slovensko", 234234L, 234234L, 46579L, "343434", "asd@asd.sd", "34sfasfa");
+        CompaniesDao mongo = CompaniesFactory.INSTANCE.getCompanyDao();
+        long daco = mongo.size();
         mongo.addCompany(company);
-        Assert.assertEquals(mongo.size(),daco+1);
+        Assert.assertEquals(mongo.size(), daco + 1);
     }
 
     @Test
     public void testGetCompanies() {
-        CompaniesDao mongo = new CompaniesFactory().getCompanyDao();
-        
+        Company company = new Company("asd", "asd", "asd", 43242, "Slovensko", 234234L, 234234L, 46579L, "343434", "asd@asd.sd", "34sfasfa");
+        CompaniesDao mongo = CompaniesFactory.INSTANCE.getCompanyDao();
+        List<Company> companies = mongo.getCompanies();
+        mongo.addCompany(company);
+        Assert.assertEquals(mongo.getCompanies().size(), companies.size() + 1);
+
     }
 
     @Test
     public void testSearchCompany_String() {
-         CompaniesDao mongo = new CompaniesFactory().getCompanyDao();
-         Company company=mongo.searchCompany("asd");
-         System.out.println(company.getCountry());
-         Assert.assertEquals("Slovensko",company.getCountry());
+        CompaniesDao mongo = CompaniesFactory.INSTANCE.getCompanyDao();
+        Company company = mongo.searchCompany("asd");
+        System.out.println(company.getCountry());
+        Assert.assertEquals("Slovensko", company.getCountry());
     }
 
-    @Test
-    public void testSearchCompany_String_String() {
-    }
-    
 }
